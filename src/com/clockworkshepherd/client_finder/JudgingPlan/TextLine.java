42ef0bfc43b37cf5420d277b35014290d4937ad6
Warwick1156 <castitas1156@gmail.com>
2022-01-09 10:01:36 +0100

    TextLine class

---
+++ src/com/clockworkshepherd/client_finder/JudgingPlan/TextLine.java
@@ -0,0 +1,11 @@
+package com.clockworkshepherd.client_finder.JudgingPlan;
+
+public class TextLine {
+    String text;
+    textClasses textClass;
+
+    public TextLine(String text, textClasses textClass) {
+        this.text = text;
+        this.textClass = textClass;
+    }
+}
